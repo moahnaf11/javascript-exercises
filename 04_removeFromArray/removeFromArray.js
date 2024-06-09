3c69b5855763d145e8530b4fd7eb0af1fcd26c3e
Ahnaf <mohammadahnaf123@gmail.com>
2024-06-09 17:03:49 +0400

    Complete 04
    Used indexOf and lastIndexOf to remove items from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrOne, ...args) {
+    args.forEach((item) => {
+        let itemPosition = arrOne.indexOf(item);
+        if (itemPosition > -1) {
+            arrOne.splice(itemPosition, 1);
+        }
+        let endPosition = arrOne.lastIndexOf(item);
+        if (endPosition > -1) {
+            arrOne.splice(endPosition, 1)
+        }
+    })
+    return arrOne;
 };
 
 // Do not edit below this line
