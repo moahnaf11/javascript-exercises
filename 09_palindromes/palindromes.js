0e0e1ad43845aaa194b40ab0b134a7c826e05a9e
Ahnaf <mohammadahnaf123@gmail.com>
2024-06-10 17:40:56 +0400

    Complete 09
    Used a helper function recursively to complete palindrome checks

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let lowerCaseWord = word.toLowerCase();
+    let punctuation = [".", ",", ":", "!", "?"];
+    let newString = "";
+    for (let letter of lowerCaseWord) {
+        if (punctuation.includes(letter)) {
+            newString += "";
+        }   else {
+            newString += letter
+        }
+    }
+    return checkpalin(newString);
 };
+
+const checkpalin = function (word) {
+    let number = word.length
+    if (number == 0 || number == 1) return true;
+    else {
+        if (word[0] == word[number - 1]) {
+            return checkpalin(word.slice(1, number - 1).trim());
+        }   else {
+            return false;
+        }
+    }
+
+}
+
+console.log(palindromes('A car, a man, a maraca.'))
 
 // Do not edit below this line
 module.exports = palindromes;
