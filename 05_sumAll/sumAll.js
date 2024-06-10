edd0ad853b101a155bad6bfc09199b08d8ac220f
Ahnaf <mohammadahnaf123@gmail.com>
2024-06-10 09:43:22 +0400

    Complete 05
    Used 2 for loops to iterate and sum all number up to the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    let i = num1;
+    if ((num1 < 0 || num2 < 0) || (typeof(num1) != typeof(7) || typeof(num2) != typeof(7))) {
+        return "ERROR";
+    }   else if (num1 > num2) {
+        i = num2;
+        for (; i <= num1; i++) {
+            total += i;
+        }
+    }   else {
+        for (; i <= num2; i++) {
+            total += i;
+        }
+    }
+    
+    return total;
 };
 
 // Do not edit below this line
