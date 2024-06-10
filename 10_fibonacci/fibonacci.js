e0d78ba103f3e3d4bf0a4112f93215f6737c80e5
Ahnaf <mohammadahnaf123@gmail.com>
2024-06-10 17:58:39 +0400

    Complete 10
    Used while loop to complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let list = [1, 1];
+    let start = 0;
+    while (true) {
+        if (num == 1 || num == 2) {
+            return 1;
+        }   else if (num == 0) {
+            return 0;
+        }   else if (num < 0) {
+            return "OOPS"
+        }   else {
+            list.push(list[start] + list[start + 1])
+            start += 1;
+            if (list.length == num) {
+                return list.at(-1);
+            }
+        }
+    }
 
 };
 
