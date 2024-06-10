a550c33c332a6ac5e71907879f3ee14b1430d60a
Ahnaf <mohammadahnaf123@gmail.com>
2024-06-10 10:43:54 +0400

    Complete 07
    Used toFixed() and the Number() to round and return number conversion from F to C and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let answer = ((5/9) * (temp - 32));
+  if (Number.isInteger(answer)) return answer;
+  else return Number(answer.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let ans = ((9/5) * temp + 32);
+  if (Number.isInteger(ans)) return ans;
+  else return Number(ans.toFixed(1));
 };
 
 // Do not edit below this line
