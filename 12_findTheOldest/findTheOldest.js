48c1b5bc4c21b689f0b10b60e0dcdb153c04ba8a
Ahnaf <mohammadahnaf123@gmail.com>
2024-06-11 09:39:11 +0400

    Complete 12
    Used forEach and object indexing to get oldest age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let newArray = [];
+    let age = undefined;
+    
+    people.forEach(details => {
+        if (details["yearOfDeath"] == undefined) {
+            age = new Date().getFullYear() - details["yearOfBirth"];
+        }   else {
+            age = details["yearOfDeath"] - details["yearOfBirth"];
+        }
+        newArray.push(age);
 
+    })
+    let oldest = Math.max(...newArray);
+    let index = newArray.indexOf(oldest);
+    return people[index];
 };
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
